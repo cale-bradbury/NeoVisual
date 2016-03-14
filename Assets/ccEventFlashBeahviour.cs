193aeb9946a22c62d2d79544278684f10b599f8e
netgrind <cale.bradbury@gmail.com>
2016-03-14 18:49:16 -0300

    adding floating starchild

---
+++ Assets/ccEventFlashBeahviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ccEventFlashBeahviour : ccEventBase {
+
+    public MonoBehaviour obj;
+    public float time = .2f;
+    float t;
+
+    protected override void OnEvent()
+    {
+        base.OnEvent();
+        obj.enabled = true;
+        t = time;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        t -= Time.deltaTime;
+        if (t < Time.deltaTime * .5f)
+        {
+            obj.enabled = false;
+        }
+	}
+}
---
+++ Assets/ccEventToggleBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ccEventToggleBehaviour : ccEventBase {
+
+    public MonoBehaviour obj;
+
+    protected override void OnEvent()
+    {
+        base.OnEvent();
+        obj.enabled = !obj.enabled;
+    }
+}

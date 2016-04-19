d54657f98081d1221c09a94f1905142928137842
connorbell <connor.n.bell@gmail.com>
2016-04-19 10:44:42 -0400

    added models to old particle scene and object swap script

---
+++ Assets/HusbandKnife/ObjectSwap.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ObjectSwap : ccEventBase {
+
+	public List<GameObject> objects;
+	public bool ChangeOnTimer;
+
+	GameObject currentObject;
+	int objIndx = 0;
+
+	void Start() {
+		currentObject = objects[objIndx];
+		currentObject.SetActive(true);
+	}
+
+	protected override void OnEvent () {
+		if (!ChangeOnTimer) {
+			currentObject.SetActive(false);
+			objIndx = (objIndx + 1) % objects.Count;
+			currentObject = objects[objIndx];
+			currentObject.SetActive(true);
+		}
+	}
+}

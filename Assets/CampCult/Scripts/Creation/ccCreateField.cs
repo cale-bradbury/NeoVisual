8873c1ebeff5bbb59cf5701c23f2e56c47c0f9f8
netgrind <cale.bradbury@gmail.com>
2016-09-19 20:42:31 -0300

    added containWithinBounds to ccCreateField, causes objects to wrap box edges

--- Assets/CampCult/Scripts/Creation/ccCreateField.cs
+++ Assets/CampCult/Scripts/Creation/ccCreateField.cs
@@ -8,6 +8,50 @@ public class ccCreateField : ccCreateGroup {
 
 	public bool center = true;
 	private bool _center;
+    public bool containWithinBounds = false;
+
+    override protected void Update()
+    {
+        base.Update();
+        if (_distance != distance)
+        {
+            _distance = distance;
+            refresh();
+        }
+        if (_center != center)
+        {
+            _center = center;
+            refresh();
+        }
+        if (!containWithinBounds)
+            return;
+        Vector3 v;
+        for (int i = 0; i < all.Count; i++)
+        {
+            v = all[i].transform.localPosition;
+            if (center)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (v[j] < -distance[j]*.5f)
+                        v[j] += distance[j]*.5f;
+                    else if (v[j] > distance[j]*.5f)
+                        v[j] -= distance[j]*.5f;
+                }
+            }
+            else{ 
+                for (int j = 0; j < 3; j++)
+                {
+                    if (v[j] < 0)
+                        v[j] += distance[j];
+                    else if (v[j] > distance[j])
+                        v[j] -= distance[j];
+                }
+            }
+
+            all[i].transform.localPosition = v;
+        }
+    }
 
 	public override Vector3 place (int i)
 	{
@@ -25,17 +69,4 @@ public class ccCreateField : ccCreateGroup {
 		_center = center;
 	}
 	
-	// Update is called once per frame
-	
-	override protected void Update () {
-		base.Update();
-		if(_distance!=distance){
-			_distance = distance;
-			refresh();
-		}
-		if(_center!=center){
-			_center = center;
-			refresh();
-		}
-	}
 }
